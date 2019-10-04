e1298398ec9c925c1aa4c6488c8cd9fde955d8e0
Sean O'Meara <51218897+sean7956@users.noreply.github.com>
2019-10-04 15:25:48 -0400

    Implemented Skateboard mileage
    Correctly made the mileageEstimate function work
    Included the ctime, cmath and cstdlib libraries

--- Skateboard.cpp
+++ Skateboard.cpp
@@ -3,14 +3,27 @@
 //
 
 #include "Skateboard.h"
+#include<cstdlib>
+#include<ctime>
+#include <cmath>
 
 Skateboard::Skateboard(string brand, string model) {
     setBrand(brand);
     setModel(model);
 }
 
-double Skateboard::mileageEstimate(double time) {
-    return 0;
+double Skateboard::mileageEstimate(double num) {
+    srand(time(0));
+    int random = rand() % 40;   //Sets up the mileage
+    random += 10;               //Adds 10 to put it into the right bounds
+    double y = double(random)/100;  //makes it so the mileage is to the right power
+    double total = num*y;       //multiples mileage by time
+    if (num > 25 and num < 250)
+    {
+        int random2 = rand() % int(floor(num/3));   //add up to 1/3 of the mileage if between 25 and 250
+        total += random2;
+    }
+    return total;
 }
 
 string Skateboard::toString() {

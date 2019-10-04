dd14b2ca1703372115363ed4dd295c94c04494f6
Sean O'Meara <51218897+sean7956@users.noreply.github.com>
2019-10-04 12:07:19 -0400

    Implemented Skateboard.cpp functions
    Implemented most of the functions
    that are used in Skateboard.cpp
    just need to do mileageEstimate

--- Skateboard.cpp
+++ Skateboard.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Skateboard.h"
+
+Skateboard::Skateboard(string brand, string model) {
+    setBrand(brand);
+    setModel(model);
+}
+
+double Skateboard::mileageEstimate(double time) {
+    return 0;
+}
+
+string Skateboard::toString() {
+    return "-> SkateBoard\n" + Vehicle::toString();
+}
+
+Skateboard::~Skateboard() = default;
+
